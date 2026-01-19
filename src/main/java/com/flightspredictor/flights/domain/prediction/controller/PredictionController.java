750013ade084840acb906186f6ad35f33747c687
Michael Ziliani <zilianimichael@gmail.com>
2026-01-18 21:46:58 -0500

    Created prediction Controller

---
+++ src/main/java/com/flightspredictor/flights/domain/prediction/controller/PredictionController.java
@@ -0,0 +1,26 @@
+package com.flightspredictor.flights.domain.prediction.controller;
+
+import com.flightspredictor.flights.domain.prediction.dto.PredictionRequest;
+import com.flightspredictor.flights.domain.prediction.dto.ModelPredictionResponse;
+import com.flightspredictor.flights.domain.prediction.service.PredictionService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/predict")
+@RequiredArgsConstructor
+public class PredictionController {
+
+    private final PredictionService predictionService;
+
+    @PostMapping
+    public ResponseEntity<ModelPredictionResponse> predict(@RequestBody @Valid PredictionRequest request) {
+
+        return ResponseEntity.ok(predictionService.predict(request));
+    }
+}
